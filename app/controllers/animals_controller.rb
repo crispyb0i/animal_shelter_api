d06c47f371f14dcd7f0d4aae4e835f16aad99f5a
David Shin <xdavidshinx@gmail.com>
2017-07-21 13:07:20 -0700

    add pagination

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -6,7 +6,7 @@ class AnimalsController < ApplicationController
     params[:random_animal]
     @animals = Animal.random_animal
   else
-    @animals = Animal.all
+    @animals = Animal.all.paginate(:page => params[:page], :per_page => 10)
   end
   json_response(@animals)
 end
@@ -17,21 +17,26 @@ end
   end
 
   def create
-    @animal = Animal.create(animal_params)
+    @animal = Animal.create!(animal_params)
     json_response(@animal)
   end
 
   def update
     @animal = Animal.find(params[:id])
     if @animal.update!(animal_params)
-    render status: 200, json: {
-      message: "Your animal has been updated successfully"
-    }
+      render status: 200, json: {
+       message: "Your animal has been updated successfully"
+     }
+   end
   end
 
   def destroy
     @animal = Animal.find(params[:id])
-    @animal.destroy
+    if @animal.destroy!
+      render status: 200, json: {
+       message: "Your animal has been updated successfully"
+     }
+   end
   end
 
   private
